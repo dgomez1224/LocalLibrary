656345dd7fb8e965faad5b38f2e40c0b5e60c62f
david1 <david1@david1-pc.mynetworksettings.com>
2022-07-07 11:31:50 -0400

    Revisions w/ formatting

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,31 +1,36 @@
 function findAccountById(accounts, id) {
-  const result = accounts.find(account => account.id === id);
+  const result = accounts.find((account) => account.id === id);
   return result;
 }
 
 function sortAccountsByLastName(accounts) {
-  const result = accounts.sort((nameA, nameB) => nameA.name.last.toLowerCase() > nameB.name.last.toLowerCase() ? 1 : -1);
+  const result = accounts.sort((nameA, nameB) =>
+    nameA.name.last.toLowerCase() > nameB.name.last.toLowerCase() ? 1 : -1
+  );
   return result;
 }
 
 function getTotalNumberOfBorrows(account, books) {
   //account.id === books.borrows.id
-  const result = books.reduce((total, book) => total + book.borrows.filter(bookId => bookId.id === account.id).length, 0);
+  const result = books.reduce(
+    (total, book) =>
+      total + book.borrows.filter((bookId) => bookId.id === account.id).length,
+    0
+  );
   return result;
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
   // book.borrows.some(borrow => !borrow.returned && borrow.id === account.id) looks thru borrows and if returned is false and the id's match for any book it returns true
-  // 
-  return (books
-    .filter(book => book.borrows[0].id === account.id & !book.borrows[0].returned)
-    .map(book => {
-      book['author'] = authors.find(author => author.id === book.authorId)
-      return book;
-    }
-
+  //
+  return books
+    .filter(
+      (book) => (book.borrows[0].id === account.id) & !book.borrows[0].returned
     )
-  )
+    .map((book) => {
+      book["author"] = authors.find((author) => author.id === book.authorId);
+      return book;
+    });
 }
 
 module.exports = {
